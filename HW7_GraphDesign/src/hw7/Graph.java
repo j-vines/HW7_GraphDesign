b5f30e6aa0824b25c0a714212b6cf461f499b25f
Jack <3jackvines@gmail.com>
2019-11-15 12:17:05 -0500

    Created Vertex.java

--- HW7_GraphDesign/src/hw7/Graph.java
+++ HW7_GraphDesign/src/hw7/Graph.java
@@ -1,5 +1,10 @@
 package hw7;
-
+/**
+ * CS333 Homework 7: Graph Design
+ * 11/18/19
+ * @author Jack Vines, Jeremy Brothers
+ *
+ */
 public interface Graph {
 
 }
--- HW7_GraphDesign/src/hw7/Vertex.java
+++ HW7_GraphDesign/src/hw7/Vertex.java
@@ -1,5 +1,53 @@
 package hw7;
+/**
+ * CS333 Homework 7: Graph Design
+ * 11/18/19
+ * @author Jack Vines, Jeremy Brothers
+ *
+ */
+public class Vertex {
+	
+	static final int WHITE = 0;
+	static final int GREY = 1;
+	static final int BLACK = 2;
+	
+	private int color;
+	private int discovered;
+	private int finished;
+	private int key;
 
-public interface Vertex {
-
+	public Vertex(int key) {
+		color = WHITE;
+		discovered = 0;
+		finished = 0;
+		this.key = key;
+	}
+	
+	public void setColor(int color) {
+		this.color = color;
+	}
+	
+	public int getColor() {
+		return color;
+	}
+	
+	public void setDiscovered(int d) {
+		discovered = d;
+	}
+	
+	public int getDiscovered() {
+		return discovered;
+	}
+	
+	public void setFinished(int f) {
+		finished = f;
+	}
+	
+	public int getFinished() {
+		return finished;
+	}
+	
+	public int getKey() {
+		return key;
+	}
 }
