5fbcd25c9906f5bcb49a2ce4c24946bb18385e9e
Jack <3jackvines@gmail.com>
2019-11-16 17:00:25 -0500

    Added time variable to Graph.java

--- HW7_GraphDesign/src/hw7/Graph.java
+++ HW7_GraphDesign/src/hw7/Graph.java
@@ -8,10 +8,13 @@ package hw7;
  */
 public class Graph implements Graphable {
 
+	private int time;
 	
+	public Graph() {
+		time = 0;
+	}
 	// 1) need array of vertex objects
 	// 2) need array of linked lists for adjacency list representation of graph (think this might be easier than adj matrix but idk)
-	// 3) need variable for time
 	
 	public void DFS() {
 		return;
